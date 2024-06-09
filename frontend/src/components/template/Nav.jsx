1a375ff288602a4dd631d5050a62d792c2dfcf01
Lucas <lgs.08lucas@gmail.com>
2024-06-09 18:10:49 -0300

    At 2024-06-09 Criado layout e estilo do Menu

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -2,9 +2,17 @@ import './Nav.css'
 import React from 'react'
 
 const Nav = props =>{
-    return <side className="menu-area">
-        Navegação
-    </side>
+    return <aside className="menu-area">
+        {/*Refatorar criando um componente para os itens de navegação*/}
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
+    </aside>
 }
 
 export default Nav;
