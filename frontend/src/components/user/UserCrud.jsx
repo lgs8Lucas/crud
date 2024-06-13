a838b569f56ea0d7f532197d5944b6bffbbf2a1a
Lucas <lgs.08lucas@gmail.com>
2024-06-13 13:05:51 -0300

    At 2024-06-13 criado formulário e função para inserir cadastros

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -40,11 +40,47 @@ export default class UserCrud extends Component {
         return list;
     }
 
+    updateField(event){
+        const user = {...this.state.user}
+        user[event.target.name] = event.target.value;
+        this.setState({user})
+    }
+
+    renderForm(){
+        return(
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md6">
+                        <div className="form-group">
+                            <label>Nome:</label>
+                            <input type="text" name="name" className="form-control" value={this.state.user.name} onChange={e => this.updateField(e)}
+                            placeholder="Digite o nome..."/>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md6">
+                        <div className="form-group">
+                            <label>Email:</label>
+                            <input type="email" name="email" className="form-control" value={this.state.user.email} onChange={e => this.updateField(e)}
+                            placeholder="Digite o email..."/>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary" onClick={e=>this.save(e)}> Salvar </button>
+                        <button className="btn btn-secondary ms-3" onClick={e=>this.clear(e)}> Cancelar </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
 
     render(){
         return (
             <Main {...headerProps}>
-                Cadastro de Usuário
+                {this.renderForm()}
             </Main>
         )
     }
